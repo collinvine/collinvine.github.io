331e4b4392fa6590e000318ac6f7ff8b269137aa
Collin Vine <collinvine@gmail.com>
2020-10-25 14:43:46 -0700

    Change family img and edit js

--- code.js
+++ code.js
@@ -1,12 +1,12 @@
 // /-------------Switch hero image ----------/
 
-const getImgEl = document.querySelector('.landing__image');
+const imageEl = document.querySelector('.landing__image');
 
 const allSpans = document.querySelectorAll('.landing__intro > p > span')
 
 const switchImg = function(ele) {
   const getImgId = ele.target.id;
-  getImgEl.src = `img/${getImgId}.jpg`;
+  imageEl.src = `img/${getImgId}.jpg`;
   console.log(getImgId, getImgEl.src);
 };
 
